a22c7e2e567bb21edb4c544e5e3f9cf512040874
Clay Ellis <clayellis@me.com>
2018-05-09 22:51:16 -0600

    Add sum and count methods to Roll. Add sum to array of Die.

--- Sources/Roll.swift
+++ Sources/Roll.swift
@@ -11,7 +11,41 @@ import Foundation
 struct Roll {
     let dice: [Die]
 
+    // MARK: Sum
+
     func sum(of die: Die) -> Int {
-        return dice.filter { $0 == die }.map { $0.rawValue }.reduce(0, +)
+        return dice.filter { $0 == die }.sum()
+    }
+
+    func sum() -> Int {
+        return dice.sum()
+    }
+
+    // MARK: Counts
+
+    func countPerDie() -> [Die: Int] {
+        var counts = [Die: Int]()
+        for die in dice {
+            counts[die, default: 0] += 1
+        }
+        return counts
+    }
+
+    func count(of die: Die) -> Int {
+        return countPerDie()[die] ?? 0
+    }
+
+    func hasCount(_ count: Int, of die: Die) -> Bool {
+        return self.count(of: die) >= count
+    }
+
+    func hasCountOfAKind(count: Int) -> Bool {
+        return countPerDie().first { $0.value >= count } != nil
+    }
+}
+
+extension Array where Element == Die {
+    func sum() -> Int {
+        return self.map { $0.rawValue }.reduce(0, +)
     }
 }
