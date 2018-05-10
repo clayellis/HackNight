72f23f06ddd888cf78305721f968a70c6a081551
Clay Ellis <clayellis@me.com>
2018-05-09 21:10:23 -0600

    Added Xcode project, gitignore.

---
+++ Sources/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yahtzee
+//
+//  Created by Clay Ellis on 5/9/18.
+//  Copyright © 2018 Clay Ellis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
