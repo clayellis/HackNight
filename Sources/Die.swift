376eb72013341de8c90747f3c96b92cd61251b67
Clay Ellis <clayellis@me.com>
2018-05-14 17:16:26 -0600

    Clean up and document Die

--- Sources/Die.swift
+++ Sources/Die.swift
@@ -8,29 +8,22 @@
 
 import Foundation
 
+/// The face value of a six-sided die.
 enum Die: Int {
     case one = 1, two, three, four, five, six
-
-    static var all: [Die] = [.one, .two, .three, .four, .five, .six]
-
-    private static let count: Die.RawValue = {
-        var nextValue: Int = 0
-        while let _ = Die(rawValue: nextValue) {
-            nextValue += 1
-        }
-        return nextValue
-    }()
-
-    static func random() -> Die {
-        let rand = Int(arc4random_uniform(UInt32(count)))
-        return Die(rawValue: rand)!
-    }
 }
 
-extension Die: ExpressibleByIntegerLiteral {
-    typealias IntegerLiteralType = Int
+extension Die {
 
-    init(integerLiteral value: IntegerLiteralType) {
-        self.init(rawValue: value)!
+    /// All possible values of `Die`.
+    static let all: [Die] = [.one, .two, .three, .four, .five, .six]
+}
+
+extension Die {
+
+    /// - returns: A randome `Die`.
+    static func random() -> Die {
+        let rand = Int(arc4random_uniform(UInt32(all.count)))
+        return Die(rawValue: rand)!
     }
 }
