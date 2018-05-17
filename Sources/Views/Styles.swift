dbf7ea5785d9ecce8a29b8e23cf07c08b8044e59
Clay Ellis <clayellis@me.com>
2018-05-17 11:08:44 -0600

    Add shadow application convenience

--- Sources/Views/Styles.swift
+++ Sources/Views/Styles.swift
@@ -6,11 +6,19 @@
 //  Copyright © 2018 Clay Ellis. All rights reserved.
 //
 
-import Foundation
-import CoreGraphics
+import UIKit
 
 struct Styles {
     private init() {}
 
     static let elementCornerRadius: CGFloat = 15
+
+    static let lightBackground = UIColor(hex: "F2F2F2")
+
+    static func applyShadow(to view: UIView) {
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.10
+        view.layer.shadowRadius = 3
+        view.layer.shadowOffset = CGSize(width: 0, height: 2)
+    }
 }
