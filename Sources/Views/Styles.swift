1eaa374f2aa2e8e61a2ea6ac38d20ba3d8a9e9e6
Clay Ellis <clayellis@me.com>
2018-05-17 10:56:54 -0600

    Add Styles to centralize common styles

---
+++ Sources/Views/Styles.swift
@@ -0,0 +1,16 @@
+//
+//  Styles.swift
+//  Yahtzee
+//
+//  Created by Clay Ellis on 5/17/18.
+//  Copyright © 2018 Clay Ellis. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Styles {
+    private init() {}
+
+    static let elementCornerRadius: CGFloat = 15
+}
