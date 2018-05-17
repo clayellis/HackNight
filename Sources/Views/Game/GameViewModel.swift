f35b23da307ea6b13e163c0742ac14b2d87b6c88
Clay Ellis <clayellis@me.com>
2018-05-17 03:03:37 -0600

    Create GameViewModel

---
+++ Sources/Views/Game/GameViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  GameViewModel.swift
+//  Yahtzee
+//
+//  Created by Clay Ellis on 5/17/18.
+//  Copyright © 2018 Clay Ellis. All rights reserved.
+//
+
+import Foundation
+
+protocol GameViewModelDelegate: class {
+    func rollDidChange(to newRoll: Roll?)
+}
+
+protocol GameViewModeling: class {
+    var delegate: GameViewModelDelegate? { get set }
+    var rollButtonTitle: String { get }
+    func handleRollTapped()
+    func handleSelectionsChange(from: Set<Int>, to: Set<Int>)
+}
+
+class GameViewModel: GameViewModeling {
+
+    weak var delegate: GameViewModelDelegate?
+
+    var currentRoll: Roll?
+
+    var currentSelections = Set<Int>()
+
+    var rollButtonTitle: String {
+        return "Roll"
+    }
+
+    func handleRollTapped() {
+        if let current = currentRoll {
+            do {
+                currentRoll = try current.reroll(savingDiceAt: currentSelections)
+            } catch {
+                print(error.localizedDescription)
+                return
+            }
+        } else {
+            currentRoll = Roll()
+        }
+
+        delegate?.rollDidChange(to: currentRoll)
+    }
+
+    func handleSelectionsChange(from: Set<Int>, to: Set<Int>) {
+        currentSelections = to
+        print("Selections: \(to.sorted())")
+    }
+}
