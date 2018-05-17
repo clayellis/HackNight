8b07227e4af9dda6d5dc87b7758b325d41a4bc02
Clay Ellis <clayellis@me.com>
2018-05-17 03:03:26 -0600

    Create GameViewController

---
+++ Sources/Views/Game/GameViewController.swift
@@ -0,0 +1,57 @@
+//
+//  GameViewController.swift
+//  Yahtzee
+//
+//  Created by Clay Ellis on 5/17/18.
+//  Copyright © 2018 Clay Ellis. All rights reserved.
+//
+
+import UIKit
+
+final class GameViewController: UIViewController {
+
+    let gameView = GameView()
+    let viewModel: GameViewModeling
+
+    init(viewModel: GameViewModeling) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        viewModel.delegate = self
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        view = gameView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        gameView.rollView.delegate = self
+        configureButtons()
+    }
+
+    private func configureButtons() {
+        gameView.rollButton.setTitle(viewModel.rollButtonTitle, for: .normal)
+        gameView.rollButton.addTarget(self, action: #selector(rollTapped), for: .touchUpInside)
+    }
+
+    @objc private func rollTapped() {
+        viewModel.handleRollTapped()
+    }
+
+}
+
+extension GameViewController: RollViewDelegate {
+    func rollView(_ rollView: RollView, didChangeSelectionsFrom from: Set<Int>, to: Set<Int>) {
+        viewModel.handleSelectionsChange(from: from, to: to)
+    }
+}
+
+extension GameViewController: GameViewModelDelegate {
+    func rollDidChange(to newRoll: Roll?) {
+        gameView.rollView.roll = newRoll
+    }
+}
