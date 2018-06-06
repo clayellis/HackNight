543743074534f6fda8e0c5147ebebd01793a3fe7
Clay Ellis <clayellis@me.com>
2018-06-06 00:01:25 -0600

    Draw the dots in DiceView instead of using subviews and autolayout

--- Sources/Views/Controls/DiceView.swift
+++ Sources/Views/Controls/DiceView.swift
@@ -13,7 +13,7 @@ final class DiceView: UIView {
 
     var dice: Die? {
         didSet {
-            configureDots()
+            setNeedsDisplay()
         }
     }
 
@@ -28,7 +28,7 @@ final class DiceView: UIView {
         return CGSize(width: size, height: size)
     }
 
-    private enum DotPosition {
+    private enum DotPosition: CaseIterable {
         case upperLeft
         case upperRight
         case lowerLeft
@@ -37,25 +37,68 @@ final class DiceView: UIView {
         case middleLeft
         case middleRight
 
-        static let all: Set<DotPosition> = [
-            .upperLeft, .upperRight, .lowerLeft, .lowerRight, .middle, .middleLeft, .middleRight
-        ]
-    }
+        enum Row {
+            case upper
+            case middle
+            case lower
+        }
 
-    private let dots: [DotPosition: DotView] = {
-        let keysValues = zip(
-            DotPosition.all,
-            DotPosition.all.map { _ in DotView() }
-        )
-        return Dictionary(uniqueKeysWithValues: keysValues)
-    }()
+        var row: Row {
+            switch self {
+            case .upperLeft, .upperRight:
+                return .upper
+            case .middleLeft, .middle, .middleRight:
+                return .middle
+            case .lowerLeft, .lowerRight:
+                return .lower
+            }
+        }
+
+        enum Column {
+            case left
+            case middle
+            case right
+        }
+
+        var column: Column {
+            switch self {
+            case .upperLeft, .middleLeft, .lowerLeft:
+                return .left
+            case .middle:
+                return .middle
+            case .upperRight, .middleRight, .lowerRight:
+                return .right
+            }
+        }
+
+        static func positions(for dice: Die) -> Set<DotPosition> {
+            switch dice {
+            case .one:
+                return [.middle]
+
+            case .two:
+                return [.lowerLeft, .upperRight]
+
+            case .three:
+                return [.lowerLeft, .middle, .upperRight]
+
+            case .four:
+                return [.upperLeft, .upperRight, .lowerLeft, .lowerRight]
+
+            case .five:
+                return [.upperLeft, .upperRight, .middle, .lowerLeft, .lowerRight]
+
+            case .six:
+                return [.upperLeft, .upperRight, .middleLeft, .middleRight, .lowerLeft, .lowerRight]
+            }
+        }
+    }
 
     init(dice: Die?) {
         self.dice = dice
         super.init(frame: .zero)
         configureAppearance()
         configureLayout()
-        configureDots()
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -69,114 +112,61 @@ final class DiceView: UIView {
         layer.cornerRadius = 10
     }
 
-    private func configureLayout() {
-        setContentCompressionResistancePriority(.required, for: .vertical)
-        setContentCompressionResistancePriority(.required, for: .horizontal)
-        
-        layoutMargins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-        var constraints = [NSLayoutConstraint]()
-
-        for (position, dotView) in dots {
-            addAutoLayoutSubview(dotView)
-            
-            switch position {
-            case .upperLeft:
-                constraints.append(dotView.leftAnchor.constraint(equalTo: layoutMarginsGuide.leftAnchor))
-                constraints.append(dotView.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor))
-
-            case .upperRight:
-                constraints.append(dotView.rightAnchor.constraint(equalTo: layoutMarginsGuide.rightAnchor))
-                constraints.append(dotView.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor))
-
-            case .middleLeft:
-                constraints.append(dotView.leftAnchor.constraint(equalTo: layoutMarginsGuide.leftAnchor))
-                constraints.append(dotView.centerYAnchor.constraint(equalTo: layoutMarginsGuide.centerYAnchor))
-
-            case .middleRight:
-                constraints.append(dotView.rightAnchor.constraint(equalTo: layoutMarginsGuide.rightAnchor))
-                constraints.append(dotView.centerYAnchor.constraint(equalTo: layoutMarginsGuide.centerYAnchor))
-
-            case .lowerLeft:
-                constraints.append(dotView.leftAnchor.constraint(equalTo: layoutMarginsGuide.leftAnchor))
-                constraints.append(dotView.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor))
-
-            case .lowerRight:
-                constraints.append(dotView.rightAnchor.constraint(equalTo: layoutMarginsGuide.rightAnchor))
-                constraints.append(dotView.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor))
-
-            case .middle:
-                constraints.append(dotView.centerXAnchor.constraint(equalTo: layoutMarginsGuide.centerXAnchor))
-                constraints.append(dotView.centerYAnchor.constraint(equalTo: layoutMarginsGuide.centerYAnchor))
-            }
-        }
-
-        NSLayoutConstraint.activate(constraints)
-    }
-
-    private func configureDots() {
-        guard let dice = dice else {
-            showDots(at: [])
-            return
-        }
-
-        switch dice {
-        case .one:
-            showDots(at: .middle)
-
-        case .two:
-            showDots(at: .lowerLeft, .upperRight)
-
-        case .three:
-            showDots(at: .lowerLeft, .middle, .upperRight)
-
-        case .four:
-            showDots(at: .upperLeft, .upperRight, .lowerLeft, .lowerRight)
-
-        case .five:
-            showDots(at: .upperLeft, .upperRight, .middle, .lowerLeft, .lowerRight)
-
-        case .six:
-            showDots(at: .upperLeft, .upperRight, .middleLeft, .middleRight, .lowerLeft, .lowerRight)
-        }
-    }
-
-    private func showDots(at positions: Set<DotPosition>) {
-        // Show the provided dots
-        positions
-            .compactMap { dots[$0] }
-            .forEach { $0.isHidden = false }
-
-        // Hide the others
-        DotPosition.all.subtracting(positions)
-            .compactMap { dots[$0] }
-            .forEach { $0.isHidden = true }
-    }
-
-    private func showDots(at positions: DotPosition...) {
-        showDots(at: Set(positions))
-    }
+    private func configureLayout() {}
 
     private func updateSelectedAppearance() {
         layer.borderColor = isSelected ? Styles.selectionBlue.cgColor : UIColor.lightGray.cgColor
         layer.borderWidth = isSelected ? 3 : 1
     }
-}
 
-private class DotView: UIView {
-    private let radius: CGFloat
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext(), let dice = dice else {
+            return
+        }
 
-    init(radius: CGFloat = 8) {
-        self.radius = radius
-        super.init(frame: .zero)
-        backgroundColor = .black
-        layer.cornerRadius = radius / 2
-    }
+        // This fill color will be used by the dots
+        context.setFillColor(UIColor.black.cgColor)
 
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+        let paddedRect = rect.inset(by: UIEdgeInsets(allEdges: rect.height * 0.15))
+        let spacing = paddedRect.height * 0.08
+        let dH = (paddedRect.height + spacing * 2) / 3
+        let dW = (paddedRect.width + spacing * 2) / 3
 
-    override var intrinsicContentSize: CGSize {
-        return CGSize(width: radius, height: radius)
+        for position in DotPosition.positions(for: dice) {
+            let top: CGFloat
+            let bottom: CGFloat
+            switch position.row {
+            case .upper:
+                top = 0
+                bottom = dH * 2
+            case .middle:
+                top = dH
+                bottom = dH
+            case .lower:
+                top = dH * 2
+                bottom = 0
+            }
+
+            let left: CGFloat
+            let right: CGFloat
+            switch position.column {
+            case .left:
+                left = 0
+                right = dW * 2
+            case .middle:
+                left = dW
+                right = dW
+            case .right:
+                left = dW * 2
+                right = 0
+            }
+
+            var rect = paddedRect.inset(by: UIEdgeInsets(top: top, left: left, bottom: bottom, right: right))
+
+            // Ensure that the rect is a square
+            rect.size.width = rect.size.height
+
+            context.fillEllipse(in: rect)
+        }
     }
 }
